845631a35561d5670594f47c2c14464abcc05633
unknown <usaydansari77@gmail.com>
2026-01-25 22:10:07 +0530

    a

---
+++ lecture-11/Circle.java
@@ -0,0 +1,13 @@
+public class Circle
+{
+	public static void main(String[] args) {
+		System.out.println("Enter radius of ur circle and area and circumference will be calculated ");
+        int radius = 10;
+        double area, circ;
+        area = Math.PI * Math.pow(radius, b : 2);
+        circ = 2 * Math.PI * radius;
+        System.out.println("area is " + area);
+        System.out.println("Circuference is " +circ);
+
+	}
+}
