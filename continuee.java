794e7ba9e44651f157aa7fd3aa29188978ea6277
unknown <usaydansari77@gmail.com>
2026-02-05 22:45:02 +0530

    First upload

---
+++ continuee.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class continuee {
+    public static void main(String[] args) {
+        int i;
+        for (i=1;i<=10;i++) {
+            if(i==1 || i==2 || i==3 || i==4 || i==5 || i==8 || i==9 || i==10)
+                continue;
+            System.out.print(i);
+
+        }
+    }
+    
+
+    
+}
