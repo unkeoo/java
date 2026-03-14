7f24ab14c69a007cad186619c279f3b37c6ddd90
unknown <usaydansari77@gmail.com>
2026-03-14 23:19:52 +0530

    .

---
+++ lecture-13/Example3.java
@@ -0,0 +1,12 @@
+ class Example3 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a= Integer.parseInt(args[0]);
+        b= Integer.parseInt(args[1]);
+        c= a + b;
+        System.out.println("the first number is" + args[0]);
+        System.out.println("the second number is" + args[1]);
+        System.out.println("the sum is" + c);
+    }
+    
+}
